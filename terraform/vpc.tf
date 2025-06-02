2042edc6e19d95e1a6d471fb0791867b2adefc42
visala123 <visala.k@algorims.com>
2025-06-02 13:11:25 +0800

    remove the comments for gateways

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -10,9 +10,9 @@ module "vpc" {
   private_subnets = ["172.20.1.0/24", "172.20.2.0/24", "172.20.3.0/24"]
   public_subnets  = ["172.20.4.0/24", "172.20.5.0/24", "172.20.6.0/24"]
 
-  # enable_nat_gateway   = true
-  # single_nat_gateway   = true
-  # enable_dns_hostnames = true
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
 
   public_subnet_tags = {
     "kubernetes.io/cluster/${local.cluster_name}" = "shared"
